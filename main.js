cea64c95a72e0df5e9610d32d43e1c9fa3912137
Bhavya Dua <dua_bhavya@outlook.com>
2022-11-02 18:31:28 -0400

    commit tree 89aef92c5dce8b7cc9d088f26207229fa8c39963

--- main.js
+++ main.js
@@ -3,9 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const hearts = document.querySelectorAll('.like-glyph');
+  for(const heart of hearts) {
+    addLikeListener(heart);
+  }
 
+  function addLikeListener(heart) {
+    heart.addEventListener('click', serverCall)
+  }
 
+  function serverCall(e) {
+    mimicServerCall()
+    .then(() => handleLike(e))
+    .catch(error => handleLikeError(error));
+  }
 
+  function handleLike(e) {
+    e.target.classList.add('activated-heart');
+    e.target.textContent = FULL_HEART;
+    e.target.removeEventListener('click', serverCall);
+    e.target.addEventListener('click', handleUnlike);
+  }
+
+  function handleUnlike(e) {
+    e.target.classList.remove('activated-heart');
+    e.target.textContent = EMPTY_HEART;
+    e.target.removeEventListener('click', handleUnlike);
+    addLikeListener(e.target);  
+  }
+
+  function handleLikeError(error) {
+    document.querySelector('#modal-message').textContent = error;
+    document.querySelector('#modal').classList.remove('hidden');
+    setTimeout(() => document.querySelector('#modal').classList.add('hidden'), 3000);
+  }
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
